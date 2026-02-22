53115a6e7427c10d02dc207174a37c6576637ca2
Vivek Mathew <vivekmathew2004@gmail.com>
2026-02-22 15:57:47 -0500

    Stupid bug

--- src/IMUHandler.h
+++ src/IMUHandler.h
@@ -9,7 +9,7 @@ public:
     // Constructor handles probing and initialization
     IMUHandler();
 
-    Eigen::Matrix<double, 6, 1> initialize();
+    Eigen::Matrix<double, 12, 1> initialize();
 
     // Reads sensor and returns 6x1 Eigen Matrix [ax, ay, az, gx, gy, gz]
     Eigen::Matrix<double, 6, 1> update();
