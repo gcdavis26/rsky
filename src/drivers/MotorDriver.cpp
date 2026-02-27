78661875bdc0fd74c46a53cdf5976500135075ed
mbp5831 <mbp5831@psu.edu>
2026-02-27 12:38:01 -0500

    .

--- src/drivers/MotorDriver.cpp
+++ src/drivers/MotorDriver.cpp
@@ -32,9 +32,11 @@ bool MotorDriver::initialize() {
     //calibrate();  shouldn't need this because it might cause the motors to command 2000 PWM if already calibrated. Need to test again. 
 
     // Arming sequence: ensure ESCs see low signal to initialize
+    /*
     for (int pin : motor_pins) {
         pwm_driver.set_duty_cycle(pin, (float)PWM_SAFE);
     }
+    */
 
     usleep(50000);
     return true;
