2d82dd7b9b9493d31c4ba421be28da577ed652cb
mbp5831 <mbp5831@psu.edu>
2026-03-22 14:33:31 -0400

    .

--- include/mocap/network.h
+++ include/mocap/network.h
@@ -3,11 +3,22 @@ int readDatalink( void );
 
 // network.h  – add this
 struct onboardMocapClient_ref {
-    unsigned char sync1, sync2, sync3, spare;
-    int          messageID, messageSize;
-    unsigned int hcsum, csum;
-    float pos_x, pos_y, pos_z;
-    float qx, qy, qz, qw;
-    int   frameNum, valid;
+	unsigned char sync1; /*  */
+	unsigned char sync2; /*  */
+	unsigned char sync3; /*  */
+	unsigned char spare; /*  */
+	int messageID; /* id # */
+	int messageSize; /* including header */
+	unsigned int hcsum; /*  */
+	unsigned int csum; /*  */
+	float pos_x; /*  */
+	float pos_y; /*  */
+	float pos_z; /*  */
+	float qx; /*  */
+	float qy; /*  */
+	float qz; /*  */
+	float qw; /*  */
+	int frameNum; /*  */
+	int valid; /*   */
 };
 extern onboardMocapClient_ref onboardMocapClient;
--- src/mocap/network.cpp
+++ src/mocap/network.cpp
@@ -67,25 +67,7 @@ struct datalinkHeader_ref datalinkHeader = {
 };
 
 #define DATALINK_MESSAGE_OPTITRACK 230
-struct onboardMocapClient_ref {
-  unsigned char sync1; /*  */
-  unsigned char sync2; /*  */
-  unsigned char sync3; /*  */
-  unsigned char spare; /*  */
-  int messageID; /* id # */
-  int messageSize; /* including header */
-  unsigned int hcsum; /*  */
-  unsigned int csum; /*  */
-  float pos_x; /*  */
-  float pos_y; /*  */
-  float pos_z; /*  */
-  float qx; /*  */
-  float qy; /*  */
-  float qz; /*  */
-  float qw; /*  */
-  int frameNum; /*  */
-  int valid; /*   */
-};
+
 struct onboardMocapClient_ref onboardMocapClient = {
   0xa1,
   0xb2,
